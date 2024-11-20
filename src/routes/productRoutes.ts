c281473626350898013cef538b38c0b71a39add9
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-11-21 02:10:52 +0700

    feat: api-docs - implement swagger for product

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -8,8 +8,14 @@ const router = express.Router();
 
 router.get("/", ProductController.GetAllProducts);
 router.get("/:id", ProductController.GetProductById);
-router.put("/:id", validate(updateProductSchema), productImg.single('image'), ProductController.UpdateProduct);
-router.post("/", validate(createProductSchema), productImg.single('image'), ProductController.CreateProduct);
+router.put("/:id", productImg.single('image'), validate(updateProductSchema), ProductController.UpdateProduct);
+router.post("/", productImg.single('image'), validate(createProductSchema), ProductController.CreateProduct);
 router.delete("/:id", ProductController.DeleteProduct);
+
+
+// swagger
+router.put("/edit/:id", productImg.single('image'), validate(updateProductSchema), ProductController.UpdateProduct);
+router.post("/add", productImg.single('image'), validate(createProductSchema), ProductController.CreateProduct);
+router.delete("/delete/:id", ProductController.DeleteProduct);
 
 export default router;
--- src/validations/productValidations.ts
+++ src/validations/productValidations.ts
@@ -4,12 +4,10 @@ export const createProductSchema = Joi.object({
     name: Joi.string().min(3).max(100).required(),
     desc: Joi.string().min(3).max(100).required(),
     category_id: Joi.number().integer().positive().required(),
-    image: Joi.string().min(3).max(200).required()
 });
 
 export const updateProductSchema = Joi.object({
-    name: Joi.string().min(3).max(100).optional(),
-    desc: Joi.string().min(3).max(100).optional(),
-    category_id: Joi.number().integer().positive().optional(),
-    image: Joi.string().min(3).max(200).optional()
+    name: Joi.string().min(3).max(100).required(),
+    desc: Joi.string().min(3).max(100).required(),
+    category_id: Joi.number().integer().positive().required(),
 });
