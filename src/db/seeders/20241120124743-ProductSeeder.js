00a736e71458d80d9ec3498f6a83924a6508c127
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-11-21 02:12:41 +0700

    feat: product - update image link dummy

--- src/db/seeders/20241120124743-ProductSeeder.js
+++ src/db/seeders/20241120124743-ProductSeeder.js
@@ -6,21 +6,21 @@ module.exports = {
     await queryInterface.bulkInsert('Products', [{
       name: 'Mikrotik RB951 2nd',
       desc: "Mikrotik RB951 2nd",
-      image: "/cloudinary/image.jpg",
+      image: "https://res.cloudinary.com/dulz7fjbz/image/upload/v1732129616/newus/pg3fzii8dsrdjynkbckm.png",
       category_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       name: 'TP LINK 1440',
       desc: "TP LINK 1440",
-      image: "/cloudinary/image.jpg",
+      image: "https://res.cloudinary.com/dulz7fjbz/image/upload/v1732129616/newus/pg3fzii8dsrdjynkbckm.png",
       category_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       name: 'TP LINK OUTDOOR',
       desc: "TP LINK OUTDOOR",
-      image: "/cloudinary/image.jpg",
+      image: "https://res.cloudinary.com/dulz7fjbz/image/upload/v1732129616/newus/pg3fzii8dsrdjynkbckm.png",
       category_id: 2,
       createdAt: new Date(),
       updatedAt: new Date()
